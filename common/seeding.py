95b0b9a057341cd6ceeaa5f5d316cdb497daa57e
Cristiano Corsi <c.corsi99@gmail.com>
2026-07-17 16:06:20 +0200

    misc

--- common/seeding.py
+++ common/seeding.py
@@ -9,6 +9,9 @@ def set_global_seed(seed: int) -> None:
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.use_deterministic_algorithms(True)
 
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
