72b2d5f15d2bd88f8654b0736c76b7b9837d1239
Cristiano Corsi <c.corsi99@gmail.com>
2026-07-17 19:17:15 +0200

    Remove call to torch.use_deterministic_algorithms in set_global_seed function due to incompatibility

--- common/seeding.py
+++ common/seeding.py
@@ -11,7 +11,6 @@ def set_global_seed(seed: int) -> None:
     torch.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-    torch.use_deterministic_algorithms(True)
 
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
